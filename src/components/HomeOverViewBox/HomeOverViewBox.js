cc03f1efc92b331202271b3f90c560992490e893
triss <cheezeitftw@gmail.com>
2023-01-03 05:29:53 -0600

    home overview tabs update

--- src/components/HomeOverViewBox/HomeOverViewBox.js
+++ src/components/HomeOverViewBox/HomeOverViewBox.js
@@ -1,5 +1,6 @@
 import { navigate } from "gatsby";
 import React from "react";
+import { BackgroundSvg } from "./BackgroundSvg";
 
 export const HomeOverViewBox = ({ title, children, textButton, icon, url }) => {
   const handleClick = (url) => {
@@ -7,21 +8,28 @@ export const HomeOverViewBox = ({ title, children, textButton, icon, url }) => {
   };
 
   return (
-    <div className="pl-2 pr-2 flex flex-col gap-4">
-      <div className="flex gap-4 items-baseline mb-1 mt-2">
-        <div className="bg-red-600 rounded:sm pt-1 pr-3 pb-1 pl-3">{icon}</div>
-        {title && <h6 className="text-lg text-gray-600 font-bold">{title}</h6>}
-      </div>
+    <div>
+      <div className="pl-2 pr-2 flex flex-col gap-4 max-w-lg">
+        <div className="flex gap-4 items-baseline mb-1 mt-2">
+          {/* <div className="bg-red-600 rounded:sm pt-1 pr-3 pb-1 pl-3">
+            {icon}
+          </div> */}
+          {/* {title && (
+            <h6 className="text-lg text-gray-600 font-bold">{title}</h6>
+          )} */}
+        </div>
 
-      <p className="text-gray-800 text-left text-base">{children}</p>
-      <button
-        onClick={() => {
-          handleClick(url);
-        }}
-        className=" bg-cyan-600 text-gray-200 active:text-gray-50 p-4 w-40 lg:hover:text-gray-50 font-semibold active:shadow-md hover:shadow-md"
-      >
-        {textButton}
-      </button>
+        <p className="p-3 text-gray-800 text-left text-base">{children}</p>
+        <button
+          onClick={() => {
+            handleClick(url);
+          }}
+          className=" mr-4 self-end align bg-cyan-600 text-gray-200 active:text-gray-50 p-4 w-40 lg:hover:text-gray-50 font-semibold active:shadow-md hover:shadow-md"
+        >
+          {textButton}
+        </button>
+      </div>
+      {/* <BackgroundSvg /> */}
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import { RiCustomerService2Fill } from "react-icons/ri";
 import { AiOutlineMail } from "react-icons/ai";
 import { IoIosPaper } from "react-icons/io";
 import { CgWebsite } from "react-icons/cg";
+import * as Tabs from "@radix-ui/react-tabs";
 
 export const Head = () => (
   <>
@@ -56,58 +57,83 @@ const IndexPage = () => {
             </div>
           </div>
         </section>
-        <section className="pb-10 bg-gray-100 ">
-          <div className="pt-4 grid grid-cols-1 gap-y-12 justify-items-center	content-center md:grid-cols-2">
-            <HomeOverViewBox
-              img={manWithLaptop}
-              title="Services"
-              url="/services"
-              textButton={"See services"}
-              icon={<RiCustomerService2Fill className="text-red-200" />}
+        <section className="pb-20 bg-gray-100 ">
+          <Tabs.Root className="min-h-[500px]" defaultValue="servicesTab">
+            <Tabs.List
+              className="flex gap-1 p-1"
+              aria-label="Overview of New Peak"
             >
-              Our team of professionals are dedicated to providing top-quality
-              service and support to meet the unique needs of our customers.
-              From Email Hosting & Management, IT Services, and Website Design,
-              we offer a range of services to help you succeed in today's
-              digital age.
-            </HomeOverViewBox>
-            <HomeOverViewBox
-              img={manWithLaptop}
-              title="Contact"
-              url="/contact"
-              textButton={"Get in contact"}
-              icon={<AiOutlineMail className="text-red-200" />}
-            >
-              Newpeak is here to be your partner in all things technology.
-              Contact us today to learn more about how we can work together to
-              meet your technology needs.
-            </HomeOverViewBox>
-            <HomeOverViewBox
-              img={manWithLaptop}
-              title="Samples"
-              url="/samples"
-              textButton={"See samples"}
-              icon={<IoIosPaper className="text-red-200" />}
-            >
-              From software development to data management, our team of experts
-              has a proven track record of delivering top-quality solutions to
-              our customers. Take a look at our samples and see for yourself the
-              level of expertise and professionalism that we bring to every
-              project.
-            </HomeOverViewBox>
-            <HomeOverViewBox
-              img={manWithLaptop}
-              title="Blog"
-              url="/blog"
-              textButton={"Read blog"}
-              icon={<CgWebsite className="text-red-200" />}
-            >
-              Stay informed about the latest in technology with our tech blog.
-              From emerging trends and innovations to practical tips and advice,
-              we've got you covered. Join us as we explore the exciting world of
-              technology.
-            </HomeOverViewBox>
-          </div>
+              <Tabs.Trigger
+                value="servicesTab"
+                className=" text-gray-800 flex flex-col gap-3 font-medium  items-start p-2 rounded-sm hover:bg-red-200"
+              >
+                <div className="text-red-200 bg-red-600   rounded:sm pt-1 pr-3 pb-1 pl-3">
+                  <RiCustomerService2Fill />
+                </div>
+                Services
+              </Tabs.Trigger>
+              <Tabs.Trigger
+                value="contactTab"
+                className=" text-gray-800 flex flex-col gap-3 font-medium items-start p-2 rounded-sm hover:bg-red-200"
+              >
+                <div className="text-red-200 bg-red-600  rounded:sm pt-1 pr-3 pb-1 pl-3">
+                  <AiOutlineMail />
+                </div>
+                Contact
+              </Tabs.Trigger>
+              <Tabs.Trigger
+                value="samplesTab"
+                className=" text-gray-800 flex flex-col gap-3 font-medium items-start p-2 rounded-sm hover:bg-red-200"
+              >
+                <div className="text-red-200 bg-red-600  rounded:sm pt-1 pr-3 pb-1 pl-3">
+                  <IoIosPaper />
+                </div>
+                Samples
+              </Tabs.Trigger>
+              <Tabs.Trigger
+                value="blogTab"
+                className="  text-gray-800 flex flex-col gap-3 font-medium items-start p-2 rounded-sm hover:bg-red-200"
+              >
+                <div className="text-red-200 bg-red-600  rounded:sm pt-1 pr-3 pb-1 pl-3">
+                  <CgWebsite />
+                </div>
+                Blog
+              </Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content value="servicesTab">
+              <HomeOverViewBox url="/services" textButton={"See services"}>
+                Our team of professionals are dedicated to providing top-quality
+                service and support to meet the unique needs of our customers.
+                From Email Hosting & Management, IT Services, and Website
+                Design, we offer a range of services to help you succeed in
+                today's digital age.
+              </HomeOverViewBox>
+            </Tabs.Content>
+            <Tabs.Content value="contactTab">
+              <HomeOverViewBox url="/contact" textButton={"Get in contact"}>
+                Newpeak is here to be your partner in all things technology.
+                Contact us today to learn more about how we can work together to
+                meet your technology needs.
+              </HomeOverViewBox>
+            </Tabs.Content>
+            <Tabs.Content value="samplesTab">
+              <HomeOverViewBox url="/samples" textButton={"See samples"}>
+                From software development to data management, our team of
+                experts has a proven track record of delivering top-quality
+                solutions to our customers. Take a look at our samples and see
+                for yourself the level of expertise and professionalism that we
+                bring to every project.
+              </HomeOverViewBox>
+            </Tabs.Content>
+            <Tabs.Content value="blogTab">
+              <HomeOverViewBox url="/blog" textButton={"Read blog"}>
+                Stay informed about the latest in technology with our tech blog.
+                From emerging trends and innovations to practical tips and
+                advice, we've got you covered. Join us as we explore the
+                exciting world of technology.
+              </HomeOverViewBox>
+            </Tabs.Content>
+          </Tabs.Root>
         </section>
       </motion.main>
     </Layout>
